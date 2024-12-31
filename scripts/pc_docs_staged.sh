eb594a9c4986e6dc1b81aa3e087e41bd5a987327
Cybergavin <cybergavin@gmail.com>
2024-12-30 16:04:20 -0800

    Initial commit

--- scripts/pc_docs_staged.sh
+++ scripts/pc_docs_staged.sh
@@ -6,12 +6,12 @@ if ! command -v terraform-docs &> /dev/null; then
     exit 1
 fi
 
-# Change to the repository root directory
-cd "$(git rev-parse --show-toplevel)"
+# Get the git repository root directory
+REPO_ROOT=$(git rev-parse --show-toplevel)
 
 # Generate docs only if .tf files in current directory are staged and README.md exists with markers
-if git diff --name-only --cached -- "*.tf" &> /dev/null && \
-   [ -f "README.md" ] && \
-   grep -q "<!-- BEGIN_TF_DOCS -->" "README.md"; then
-    terraform-docs .
+if git diff --name-only --cached -- "${REPO_ROOT}/*.tf" &> /dev/null && \
+   [ -f "${REPO_ROOT}/README.md" ] && \
+   grep -q "<!-- BEGIN_TF_DOCS -->" "${REPO_ROOT}/README.md"; then
+    terraform-docs $REPO_ROOT
 fi
