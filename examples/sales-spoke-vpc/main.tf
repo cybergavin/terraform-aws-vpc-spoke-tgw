d4cff191172acf7c17388fa16fb833cd29743143
Cybergavin <cybergavin@gmail.com>
2025-01-08 11:30:00 -0800

    fix: Example module calling

--- examples/sales-spoke-vpc/main.tf
+++ examples/sales-spoke-vpc/main.tf
@@ -1,6 +1,6 @@
 # Sales Account Networking
 module "sales-network" {
-  source                     = "../../../"
+  source                     = "../../../terraform-aws-vpc-spoke-tgw"
   org                        = var.org
   app_id                     = var.app_id
   environment                = var.environment
